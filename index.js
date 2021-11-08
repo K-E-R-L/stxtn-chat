c2aba2746c044e113db93b84fec6a40e926ba159
K E R L <61601067+K-E-R-L@users.noreply.github.com>
2021-11-08 12:49:08 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
-import React, { useState, createContext } from "react";
+import React, { useContext } from "react";
 
-export const Context = createContext();
+ import { Context } from "../context";
 
-export const ContextProvider = (props) => {
-  const [username, setUsername] = useState("");
-  const [secret, setSecret] = useState("");
+ import { useRouter } from "next/router";
 
-  const value = {
-    username,
-    setUsername,
-    secret,
-    setSecret,
-  };
+ import axios from "axios";
 
-  return <Context.Provider value={value}>{props.children}</Context.Provider>;
-  };
+ const Auth = () => {
+   const { username, setUsername, secret, setSecret } = useContext(Context);
+
+   const router = useRouter();
+
+   function onSubmit(e) {
+     e.preventDefault();
+
+     if (username.length === 1 || secret.length === 1) return;
+
+     axios
+       .put(
+         "https://api.chatengine.io/users/",
+         { username, secret },
+         { headers: { "Private-Key": "5d9d572b-8790-430d-b138-79ee15e1c63e" } }
+       )
+       .then((r) => {
+         router.push("/chats");
+       });
+   }
+
+   return (
+     <div className="background">
+       <div className="auth-container">
+         <form className="auth-form" onSubmit={(e) => onSubmit(e)}>
+           <div className="auth-title">NextJS Chat</div>
+
+           <div className="input-container">
+             <input
+               placeholder="Email"
+               className="text-input"
+               onChange={(e) => setUsername(e.target.value)}
+             />
+           </div>
+
+           <div className="input-container">
+             <input
+               type="password"
+               placeholder="Password"
+               className="text-input"
+               onChange={(e) => setSecret(e.target.value)}
+             />
+           </div>
+
+           <button type="submit" className="submit-button">
+             Login / Sign Up
+           </button>
+         </form>
+       </div>
+     </div>
+   );
+ };
+
+ export default Auth;
